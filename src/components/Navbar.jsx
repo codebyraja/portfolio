f80a04edca4a3d5da59e42822dc70e708fa585dd
Raja Singh <rajaisexcellent@gmail.com>
2025-06-13 16:58:14 +0530

    `Fixed whitespace in Navbar logo text`

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = () => {
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
           <p className="text-white text-[18px] font-bold cursor-pointer flex">
-            Raja &nbsp Singh &nbsp;
+            Raja&nbsp;Singh &nbsp;
             <span className="sm:block hidden text-white">| Developer</span>
           </p>
         </Link>
